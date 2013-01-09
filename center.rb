afcc4dc0786279a93fb179ea133fe0c9d5dd875a
tenyawanya <ikd.ipn@gmail.com>
2013-01-09 13:59:19 +0900

    center test

---
+++ center.rb
@@ -0,0 +1,7 @@
+lineWidth = 50
+puts(              'ハバードのおばさんが'.center(lineWidth))
+puts(              '食器棚の中に座ったよ'.center(lineWidth))
+puts(        'カードとホエーを食べながら'.center(lineWidth))
+puts(            '蜘蛛がそばにやってきて'.center(lineWidth))
+puts(            'おばさんの隣に座ったよ'.center(lineWidth))
+puts('かわいそうな犬は一目散に逃げ出した'.center(lineWidth))
