53a7450881eee9ddbd8ea5f55b3a81ebe22419c1
tenyawanya <ikd.ipn@gmail.com>
2013-01-09 12:12:27 +0900

    chomp2 test

---
+++ chomp2.rb
@@ -0,0 +1,8 @@
+puts 'こんにちは！ 君の名前は何かな?'
+name = gets.chomp
+puts '君の名前は ' + name + 'だね. 良い名前じゃないか!'
+name2 = name
+puts 'では、TwitterID は何かな?'
+name = gets.chomp
+puts 'TwitterID は ' + name + 'だね.残念だけどフォローしていないよ'
+puts '会えてうれしいよ, ' + name2 + '.  :)'
