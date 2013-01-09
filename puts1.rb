4c6ceffaec5069893800ea0447e01737a02eaa9e
tenyawanya <ikd.ipn@gmail.com>
2013-01-09 10:51:36 +0900

    puts test1

---
+++ puts1.rb
@@ -0,0 +1,10 @@
+puts '15'.to_f
+puts '99.999'.to_f
+puts '99.999'.to_i
+puts ''
+puts '5 は僕の好きな数!'.to_i
+puts '誰から 5が好きだってきいたの?'.to_i
+puts '君のママからだよ'.to_f
+puts ''
+puts '文字列っぽい'.to_s
+puts 3.to_i
