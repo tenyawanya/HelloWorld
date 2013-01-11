c085fa4c6df2c4e03190d731eabb637b87ac4c1e
tenyawanya <ikd.ipn@gmail.com>
2013-01-11 18:09:04 +0900

    if3 test2

--- if3.rb
+++ if3.rb
@@ -4,7 +4,7 @@ puts 'おうむ返しスクリプトです。'
   puts 'コマンドを入力して下さい(終了するには exit と入力してください)'
 command = gets.chomp
 
-while (command != 'exit' or command != 'exit')
+while command != 'exit'
   puts command
   command = gets.chomp
 end
