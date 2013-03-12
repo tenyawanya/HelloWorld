687f341ca617f6478ca2c3f9679f9972047ccc48
Kazuya NUMATA <numata@gmail.com>
2013-03-12 13:05:40 +0900

    Add a magic comment and support encoding specification on Ruby >= 1.9
    マジックコメント＆Ruby1.9によるreverse時の文字化けの回避

--- rev.rb
+++ rev.rb
@@ -1,6 +1,11 @@
+# encoding: utf-8
+
+# Ruby1.9以降では、先頭行にマジックコメントで文字コードを指定することで、
+# reverseしても文字化けしません。
+
 var1 = 'stop'
 var2 = 'stressed'
-var3 = 'にほんごはばけるのね'
+var3 = 'にほんごはばけるのね(1.8までね)'
 
 puts var1.reverse
 puts var2.reverse
